db9749c42356586001e1f448ceb2361992132e3c
Marek Olšák <m_olsak@utb.cz>
2021-11-13 23:29:12 +0100

    Page layout added.

--- CSharp_RSA_Cipher_WPF/Views/GeneratorPage.xaml.cs
+++ CSharp_RSA_Cipher_WPF/Views/GeneratorPage.xaml.cs
@@ -10,29 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace CSharp_RSA_Cipher_WPF.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindowView.xaml
+    /// Interaction logic for GeneratorPage.xaml
     /// </summary>
-    public partial class MainWindowView : Window
+    public partial class GeneratorPage : Page
     {
-        public MainWindowView()
+        public GeneratorPage()
         {
             InitializeComponent();
-        }
-
-        private void Exit_OnClick(object sender, MouseEventArgs e) => Close();
-
-        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (!e.ChangedButton.Equals(MouseButton.Left))
-            {
-                return;
-            }
-            DragMove();
         }
     }
 }
